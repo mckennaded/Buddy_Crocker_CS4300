374c18b5f5a11159fb77843c364774903a8d6efb
BenDaHen <bennyhen121@gmail.com>
2025-10-23 23:27:11 +0000

    Finished basic API implementation, needs testing

--- Buddy_Crocker/services/usda_api.py
+++ Buddy_Crocker/services/usda_api.py
@@ -2,6 +2,30 @@ import os
 from dotenv import load_dotenv
 import requests
 
+"""
+Values from the API:
+"description"   - The name of the food item
+"dataType"      - The data type of the food item
+"fdcId"         - The USDA Food ID of the food item
+"brandOwner"    - The brand of the food
+"foodNutrients" - The nutrients of the food
+"nutrientName"  - The name of the nutrient (Energy is calories)
+"value"         - The calorie count 
+"""
+
+"""
+How to use:
+
+Ensure that the USDA API key is in the .env file:
+USDA_API_KEY = "your_key"
+
+search_foods() returns the first 10 entries for the
+inputed query in the "query" field
+
+For each entry, the Description, Data Type, FDC ID, Brand, 
+and Calories are returned
+"""
+
 #Load the .env file and get the API key
 load_dotenv()
 API_KEY = os.getenv("USDA_API_KEY")
@@ -26,14 +50,46 @@ def search_foods(query, page_size=10):
 
     for food in foods:
         print("Description:", food["description"])
-        print("Data type", food["dataType"])
+        print("Data Type:", food["dataType"])
         print("FDC ID:", food["fdcId"])
         print("Brand:", food.get("brandOwner", "N/A"))
-        print("Nutrients:")
-        for nutrient in food.get("foodNutrients", []):
-            print(f"  - {nutrient['nutrientName']}: {nutrient['value']} {nutrient['unitName']}")
+        calories = next(
+            (n["value"] for n in food["foodNutrients"] if n["nutrientName"] == "Energy"),
+            None
+        )
+        print("Calories:", calories)
         print("-" * 40)
+
+    return foods
+
+def get_food_name(query, page_size=1):
+    #Set up parameters for search
+    url = 'https://api.nal.usda.gov/fdc/v1/foods/search'
+
+    params = {
+        "api_key": API_KEY,
+        "query": query,
+        "pageSize": page_size,
+    }
+
+    #Get the response from the API
+    response = requests.get(url, params=params)
+    data = response.json() #Convert to a python dictionary
+
+    #Print out info
+    foods = data["foods"]
+
+    for food in foods:
+        description = food["description"]
+        print("Description:", food["description"])
+
+    print("-" * 40)
+
+    return description
 
 #Test
 #search_foods("Cheddar Cheese")
 search_foods("Bacon")
+
+get_food_name("Cheddar Cheese")
+get_food_name("Bacon")
