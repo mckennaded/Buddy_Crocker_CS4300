2e447d8d1afcc82a2d5d7c5557a607f418f2fd80
Brianne <leathbk1512@gmail.com>
2025-11-09 19:26:58 +0000

    added auth_login import

--- Buddy_Crocker/Buddy_Crocker/views.py
+++ Buddy_Crocker/Buddy_Crocker/views.py
@@ -8,6 +8,7 @@ from .models import Allergen, Ingredient, Pantry, Profile, Recipe
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth import login as auth_login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView
